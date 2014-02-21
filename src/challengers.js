8e472be5b2305f10902bc152117514848b6219b7
ratopi <ratopi@abwesend.de>
2014-02-21 17:23:32 +0100

    challengers are now create by function

--- src/challengers.js
+++ src/challengers.js
@@ -1,124 +1,128 @@
 
-var challengers = {
+var challengers =
+	function()
+	{
+		return {
 
-	"?m": {
-		"name": "Multiplikation",
-		"fn":  function()
-		{
-			var a = Math.ceil( Math.random() * 10 );
-			var b = Math.ceil( Math.random() * 10 );
-			var c = a * b;
-			return {
-				"task": a + " · " + b,
-				"result": c
-				};
-		}
-	},
+			"?m": {
+				"name": "Multiplikation",
+				"fn":  function()
+				{
+					var a = Math.ceil( Math.random() * 10 );
+					var b = Math.ceil( Math.random() * 10 );
+					var c = a * b;
+					return {
+						"task": a + " · " + b,
+						"result": c
+						};
+				}
+			},
 
-	"?m100": {
-		"name": "Multiplikation (mit Zehnern)",
-		"fn":  function()
-		{
-			var a = Math.ceil( Math.random() * 10 );
-			var b = Math.ceil( Math.random() * 10 );
+			"?m100": {
+				"name": "Multiplikation (mit Zehnern)",
+				"fn":  function()
+				{
+					var a = Math.ceil( Math.random() * 10 );
+					var b = Math.ceil( Math.random() * 10 );
 
-			Math.random() < .5 ? a *= 10 : b *= 10;
+					Math.random() < .5 ? a *= 10 : b *= 10;
 
-			var c = a * b;
-			return {
-				"task": a + " · " + b,
-				"result": c
-				};
-		}
-	},
+					var c = a * b;
+					return {
+						"task": a + " · " + b,
+						"result": c
+						};
+				}
+			},
 
-	"?d": {
-		"name": "Division",
-		"fn": function()
-		{
-			var a = Math.ceil( Math.random() * 10 );
-			var b = Math.ceil( Math.random() * 10 );
-			var c = a * b;
-			return {
-				"task": c + " : " + b,
-				"result": a
-			};
-		}
-	},
+			"?d": {
+				"name": "Division",
+				"fn": function()
+				{
+					var a = Math.ceil( Math.random() * 10 );
+					var b = Math.ceil( Math.random() * 10 );
+					var c = a * b;
+					return {
+						"task": c + " : " + b,
+						"result": a
+					};
+				}
+			},
 
-	"?d100": {
-		"name": "Division (mit Zehnern)",
-		"fn": function()
-		{
-			var a = Math.ceil( Math.random() * 10 );
-			var b = Math.ceil( Math.random() * 10 );
+			"?d100": {
+				"name": "Division (mit Zehnern)",
+				"fn": function()
+				{
+					var a = Math.ceil( Math.random() * 10 );
+					var b = Math.ceil( Math.random() * 10 );
 
-			Math.random() < .5 ? a *= 10 : b *= 10;
+					Math.random() < .5 ? a *= 10 : b *= 10;
 
-			var c = a * b;
-			return {
-				"task": c + " : " + b,
-				"result": a
-			};
-		}
-	},
+					var c = a * b;
+					return {
+						"task": c + " : " + b,
+						"result": a
+					};
+				}
+			},
 
-	"?dr": {
-		"name": "Division mit Rest",
-		"fn": function()
-		{
-			var a = Math.ceil( Math.random() * 10 );
-			var b = Math.ceil( Math.random() * 10 );
+			"?dr": {
+				"name": "Division mit Rest",
+				"fn": function()
+				{
+					var a = Math.ceil( Math.random() * 10 );
+					var b = Math.ceil( Math.random() * 10 );
 
-			if ( Math.random() < .5 ) a *= 10;
+					if ( Math.random() < .5 ) a *= 10;
 
-			var c = Math.floor( a / b );
-			var rest = a % b;
+					var c = Math.floor( a / b );
+					var rest = a % b;
 
-			var expected = [];
-			if ( rest === 0 ) expected.push( "" + c );
-			expected.push( c + "R" + rest );
-			if ( c === 0 ) expected.push( "R" + rest );
+					var expected = [];
+					if ( rest === 0 ) expected.push( "" + c );
+					expected.push( c + "R" + rest );
+					if ( c === 0 ) expected.push( "R" + rest );
 
-			return {
-				"task": a + " : " + b,
-				"result": expected
-			};
-		}
-	},
+					return {
+						"task": a + " : " + b,
+						"result": expected
+					};
+				}
+			},
 
-	"?a": {
-		"name": "Addition",
-		"fn": function()
-		{
-			var a = Math.floor( Math.random() * 10 );
-			var b = Math.floor( Math.random() * 10 );
+			"?a": {
+				"name": "Addition",
+				"fn": function()
+				{
+					var a = Math.floor( Math.random() * 10 );
+					var b = Math.floor( Math.random() * 10 );
 
-			var expected = a + b;
+					var expected = a + b;
 
-			return {
-				"task": a + " + " + b,
-				"result": expected
-			};
-		}
-	},
+					return {
+						"task": a + " + " + b,
+						"result": expected
+					};
+				}
+			},
 
-	"?s": {
-		"name": "Subtraktion",
-		"fn": function()
-		{
-			var a = Math.ceil( Math.random() * 10 );
-			var b = Math.ceil( Math.random() * 10 );
+			"?s": {
+				"name": "Subtraktion",
+				"fn": function()
+				{
+					var a = Math.ceil( Math.random() * 10 );
+					var b = Math.ceil( Math.random() * 10 );
 
-			var c = a + b;
-			
-			var expected = c - a;
-			
-			return {
-				"task": c + " - " +a,
-				"result": expected
-			};
-		}
-	},
+					var c = a + b;
 
-};
+					var expected = c - a;
+
+					return {
+						"task": c + " - " +a,
+						"result": expected
+					};
+				}
+			},
+
+		};
+	}();
